c9ef88cd7cbe5e88d62893b9e46070e07ab7ba2b
Emanuel <101946589+mano3queijos@users.noreply.github.com>
2023-08-12 14:41:53 -0300

    animated

--- main.js
+++ main.js
@@ -1,29 +1,29 @@
-const heartIcon = document.getElementById("heart");
+const hearth = document.getElementById("heart");
 const hiddenContent = document.getElementById("hidden-content");
 
 let isUnlocked = false; // Inicialmente, a área está bloqueada
 
-heartIcon.addEventListener("click", function() {
+hearth.addEventListener("click", function() {
     if (!isUnlocked) {
-        hiddenContent.style.display = "block"; // Torna o conteúdo oculto visível
+        hiddenContent.style.display = "block"; // unlock the content
         isUnlocked = true;
-        heartIcon.classList.remove("fa-heart");
-        heartIcon.classList.add("fa-unlock"); // Substitua pelo ícone de "desbloqueado"
+
+       
     }
 });
 
 
 
-heartIcon.addEventListener("click", function() {
-    heartIcon.style.transition = "transform 0.5s ease, opacity 0.5s ease"; // Adiciona transições suaves
+hearth.addEventListener("click", function() {
+    hearth.style.transition = "transform 1s ease, opacity 1.5s ease"; // Adiciona transições suaves
 
     // Aplica transformação para crescimento e diminuição gradual da opacidade
-    heartIcon.style.transform = "scale(1.6) rotate(-45deg)"; // Aumenta o tamanho do coração
-    heartIcon.style.opacity = "0";
+    hearth.style.transform = "scale(3) rotate(-45deg)"; // Aumenta o tamanho do coração
+    hearth.style.opacity = "0";
 
     // Espera até que a transição seja concluída antes de ocultar o coração
     setTimeout(function() {
-        heartIcon.style.display = "none";
+        hearth.style.display = "none";
     }, 500); // Ajuste o tempo para coincidir com a duração da transição em milissegundos
 });
 
