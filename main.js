cdf30ab3981a676614d4f2de77afc86fcc5bbb7a
Emanuel <101946589+mano3queijos@users.noreply.github.com>
2023-08-12 16:27:28 -0300

    almost complet

--- main.js
+++ main.js
@@ -13,7 +13,6 @@ hearth.addEventListener("click", function() {
 });
 
 
-
 hearth.addEventListener("click", function() {
     hearth.style.transition = "transform 2s ease, opacity 1.5s ease"; // Adiciona transições suaves
 
@@ -27,6 +26,26 @@ hearth.addEventListener("click", function() {
     }, 500); // Ajuste o tempo para coincidir com a duração da transição em milissegundos
 });
 
+function generateRandomCompatibility(){
+    return Math.floor(Math.random() * 101);
 
+}
 
+function calculateCompatibility(name1, name2) {
+    const randomCompatibility = generateRandomCompatibility();
+    return randomCompatibility;
+}
 
+document.getElementById("calculateBtn").addEventListener("click", function() {
+    const name1 = document.getElementById("name1").value;
+    const name2 = document.getElementById("name2").value;
+
+    if (name1.trim() === "" || name2.trim() === "") {
+        alert("Por favor, insira os dois nomes para calcular a compatibilidade.");
+        return;
+    }
+
+    const compatibility = calculateCompatibility(name1, name2);
+    const resultElement = document.getElementById("result");
+    resultElement.textContent = `A compatibilidade entre ${name1} e ${name2} é de ${compatibility}%`;
+});
